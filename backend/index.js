2707f70d2a9f757c8998c5e79a267779ab0665dd
Alper Erdogan <alpererdogan8@gmail.com>
2024-02-20 19:34:48 +0300

    feat: restructured express application to include albums routes, added redirect from root to /albums endpoint.

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,14 @@
 import express from "express";
+import albums from "./routes/albums.js";
 
 const PORT = process.env.PORT;
 
 const app = express();
+
+app.use("/albums", albums);
+
 app.get("/", (req, res) => {
-  res.json({ ok: 200 });
+  res.redirect("/albums");
 });
 
 app.listen(PORT, () => {
