9b79cb6e195004e5dface9b098f411cf99efd662
Alper Erdogan <alpererdogan8@gmail.com>
2024-02-21 04:29:46 +0300

    refactor: removed comments

--- backend/routes/search.js
+++ backend/routes/search.js
@@ -11,12 +11,9 @@ search.get("/search", async (req, res) => {
       return res.redirect("/albums");
     }
     const { data } = await SearchService.query(type, search);
-
-    // console.log(data[0].photos);
     paginate(data[0].photos)(req, res, () => {
       res.json({ title: data[0].title, ...res.paginatedResults });
     });
-    // res.json(data[0].photos);
   } catch (error) {
     console.error("Error during search:", error.message);
     res.status(403).json({ error: "An error occurred during the search process." });
