32c2b65a9b48baec6ff1ce59b21da137c11a7652
Alper Erdogan <alpererdogan8@gmail.com>
2024-02-21 10:26:51 +0300

    feat: implement an Input component using class-variance-authority for variant handling and custom styling.

---
+++ frontend/src/components/atomic/input.tsx
@@ -0,0 +1,18 @@
+import { cva, type VariantProps } from "class-variance-authority";
+import { FC, ForwardedRef, forwardRef } from "react";
+import { cn } from "../../utils/utils";
+
+const inputVariants = cva(
+  "h-10 min-w-[14rem] min-h-[2.5rem] rounded-lg border-2 border-input bg-background px-3 py-2 text-base ring-offset-background placeholder:text-muted-foreground selection:bg-black selection:text-white",
+);
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement>, VariantProps<typeof inputVariants> {
+  ref?: ForwardedRef<HTMLInputElement>;
+}
+export const Input: FC<InputProps> = forwardRef(({ className, ...props }, ref) => {
+  return (
+    <>
+      <input ref={ref} className={cn(inputVariants({ className }), "peer")} {...props} />
+    </>
+  );
+});
