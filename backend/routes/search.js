8ffe677ef942944b8e98d9d5f139329ee6bdbda8
Alper Erdogan <alpererdogan8@gmail.com>
2024-02-21 04:14:37 +0300

    refactor: updated express route for search to use pagination middleware, providing paginated results for photos in the search query.

--- backend/routes/search.js
+++ backend/routes/search.js
@@ -1,19 +1,25 @@
 import { Router } from "express";
+import { paginate } from "../middlewares/paginationMiddleware.js";
 import SearchService from "../services/SearchService.js";
 
 const search = Router();
 
-search.get("/", async (req, res) => {
+search.get("/search", async (req, res) => {
   try {
     const { type, search } = req.query;
     if (type === undefined && search === undefined) {
       return res.redirect("/albums");
     }
     const { data } = await SearchService.query(type, search);
-    res.send(data);
+
+    // console.log(data[0].photos);
+    paginate(data[0].photos)(req, res, () => {
+      res.json({ title: data[0].title, ...res.paginatedResults });
+    });
+    // res.json(data[0].photos);
   } catch (error) {
     console.error("Error during search:", error.message);
-    res.status(403).send({ error: "An error occurred during the search process." });
+    res.status(403).json({ error: "An error occurred during the search process." });
   }
 });
 
