70e654c713ac5f7573e6c9762bfc66045ca9370e
Alper Erdogan <alpererdogan8@gmail.com>
2024-02-21 15:15:47 +0300

    feat: implemented Navbar component with Logo, title, and conditional rendering of Search component based on route

---
+++ frontend/src/components/organisms/navbar.tsx
@@ -0,0 +1,20 @@
+
+import { Logo } from "../../assets/logo";
+import Search from "../molecules/search";
+const Navbar = () => {
+  const route = "album-name";
+  return (
+    <nav className="w-full shadow-md py-4 px-1  md:px-8 md:py-3.5 flex items-center justify-between md:min-h-[5.5rem] ">
+      <div className="flex justify-between px-1 md:gap-8 items-center   w-full">
+        <div className="flex items-center gap-2  md:gap-8">
+          <Logo className="w-8 md:w-auto" />
+
+          <div className=" text-xl md:text-2xl font-semibold"> Albums</div>
+        </div>
+        {route === "album-name" ? <Search /> : null}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
