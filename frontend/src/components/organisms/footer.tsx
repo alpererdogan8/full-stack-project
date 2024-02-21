bc1f02d6f5e20b28e3f91b60e77dad67640d6429
Alper Erdogan <alpererdogan8@gmail.com>
2024-02-21 15:15:12 +0300

    feat: integrate Footer component with Logo in the app

---
+++ frontend/src/components/organisms/footer.tsx
@@ -0,0 +1,13 @@
+import { Logo } from "../../assets/logo";
+
+const Footer = () => {
+  return (
+    <footer
+      className="w-full fill-white
+     h-36 mt-8 flex justify-center items-center text-white bg-primary">
+      <Logo />
+    </footer>
+  );
+};
+
+export default Footer;
