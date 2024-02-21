7bfc26b9708d01ea20b5c6be1e6a677010d71040
Alper Erdogan <alpererdogan8@gmail.com>
2024-02-21 11:13:26 +0300

    feat: update Input component styling to be responsive, adjusting the padding for medium-sized screens.

--- frontend/src/components/atomic/input.tsx
+++ frontend/src/components/atomic/input.tsx
@@ -3,7 +3,7 @@ import { FC, ForwardedRef, forwardRef } from "react";
 import { cn } from "../../utils/utils";
 
 const inputVariants = cva(
-  "h-10 min-w-[14rem] min-h-[2.5rem] rounded-lg border-2 border-input bg-background px-3 py-2 text-base ring-offset-background placeholder:text-muted-foreground selection:bg-black selection:text-white",
+  "h-10  md:min-w-[14rem] min-h-[2.5rem] rounded-lg border-2 border-input bg-background md:px-3 md:py-2 text-base ring-offset-background placeholder:text-muted-foreground selection:bg-black selection:text-white",
 );
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement>, VariantProps<typeof inputVariants> {
