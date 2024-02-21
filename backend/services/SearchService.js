972466a10e186a1b894bcfae7a6bf88639d97c8a
Alper Erdogan <alpererdogan8@gmail.com>
2024-02-21 04:16:57 +0300

    refactor: enhanced documentation for the SearchService class, providing detailed comments for constructor and query method.

--- backend/services/SearchService.js
+++ backend/services/SearchService.js
@@ -1,14 +1,28 @@
 import API from "../API/ApiServices.js";
 import BaseService from "./BaseService.js";
 
+/**
+ * Service class for handling search-related queries.
+ *
+ * @extends BaseService
+ */
 class SearchService extends BaseService {
+  /**
+   * Constructor for SearchService.
+   *
+   * @param {object} APIConfig - Configuration for the API service.
+   */
   constructor(APIConfig) {
     super(APIConfig);
   }
+
   /**
+   * Executes a search query based on the specified search type and text.
+   *
    * @param {"albums"| "albums-photos" | "photos"} searchType - The type of search (either "albums" or "photos").
    * @param {string} searchText - The text to search.
    * @returns {Promise<void>} - A Promise that resolves when the query is complete.
+   * @throws {Error} - Throws an error if the searchType is invalid.
    */
   async query(searchType, searchText) {
     switch (searchType) {
