50deb1ae1b57b6cbdfc646a2dacf5ceae6a8ac42
Alper Erdogan <alpererdogan8@gmail.com>
2024-02-21 03:51:43 +0300

    feat: added search route and adjusted route handling in the express application.

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,13 @@
 import express from "express";
 import albums from "./routes/albums.js";
+import search from "./routes/search.js";
 
 const PORT = process.env.PORT;
 
 const app = express();
 
+app.use("/", search);
 app.use("/albums", albums);
-
-app.get("/", (req, res) => {
-  res.redirect("/albums");
-});
 
 app.listen(PORT, () => {
   console.log(`Starting http://localhost:${PORT}`);
