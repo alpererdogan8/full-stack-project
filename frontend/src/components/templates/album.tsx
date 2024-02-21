c497ccfb84a7d931e9164a8e12fe3f414020a126
Alper Erdogan <alpererdogan8@gmail.com>
2024-02-21 15:17:43 +0300

    feat: created Album component with multiple Card elements displaying album information

---
+++ frontend/src/components/templates/album.tsx
@@ -0,0 +1,22 @@
+import Card from "../molecules/card";
+
+const Album = () => {
+  return (
+    <div className="w-11/12 mt-10 h-auto gap-2 flex flex-col md:flex-row md:flex-wrap md:justify-center md:items-start items-center ">
+      <Card title="quidem molestiae enim" href="#" src="https://via.placeholder.com/600/771796" />
+      <Card title="quidem molestiae enim" href="#" src="https://via.placeholder.com/600/771796" />
+      <Card title="quidem molestiae enim" href="#" src="https://via.placeholder.com/600/771796" />
+      <Card title="quidem molestiae enim" href="#" src="https://via.placeholder.com/600/771796" />
+      <Card title="quidem molestiae enim" href="#" src="https://via.placeholder.com/600/771796" />
+      <Card title="quidem molestiae enim" href="#" src="https://via.placeholder.com/600/771796" />
+      <Card title="quidem molestiae enim" href="#" src="https://via.placeholder.com/600/771796" />
+      <Card title="quidem molestiae enim" href="#" src="https://via.placeholder.com/600/771796" />
+      <Card title="quidem molestiae enim" href="#" src="https://via.placeholder.com/600/771796" />
+      <Card title="quidem molestiae enim" href="#" src="https://via.placeholder.com/600/771796" />
+      <Card title="quidem molestiae enim" href="#" src="https://via.placeholder.com/600/771796" />
+      <Card title="quidem molestiae enim" href="#" src="https://via.placeholder.com/600/771796" />
+    </div>
+  );
+};
+
+export default Album;
---
+++ frontend/src/components/templates/albums.tsx
@@ -0,0 +1,22 @@
+import Card from "../molecules/card";
+
+const Albums = () => {
+  return (
+    <div className="w-11/12 my-10 h-auto gap-2 flex flex-col md:flex-row md:flex-wrap md:justify-center md:items-start items-center ">
+      <Card title="quidem molestiae enim" href="#" />
+      <Card title="quidem molestiae enim" href="#" />
+      <Card title="quidem molestiae enim" href="#" />
+      <Card title="quidem molestiae enim" href="#" />
+      <Card title="quidem molestiae enim" href="#" />
+      <Card title="quidem molestiae enim" href="#" />
+      <Card title="quidem molestiae enim" href="#" />
+      <Card title="quidem molestiae enim" href="#" />
+      <Card title="quidem molestiae enim" href="#" />
+      <Card title="quidem molestiae enim" href="#" />
+      <Card title="quidem molestiae enim" href="#" />
+      <Card title="quidem molestiae enim" href="#" />
+    </div>
+  );
+};
+
+export default Albums;
