c89d70b86ed51e9ccf6ea27127e16ff6f7112138
Alper Erdogan <alpererdogan8@gmail.com>
2024-02-21 10:26:20 +0300

    feat: implement an Image component using class-variance-authority for variant handling and custom styling.

---
+++ frontend/src/components/atomic/image.tsx
@@ -0,0 +1,14 @@
+import { cva, type VariantProps } from "class-variance-authority";
+import { FC } from "react";
+import { cn } from "../../utils/utils";
+
+const inputVariants = cva("rounded-xl");
+
+interface ImgProps extends React.ImgHTMLAttributes<HTMLImageElement>, VariantProps<typeof inputVariants> {}
+export const Image: FC<ImgProps> = ({ className, src, width, height, ...props }) => {
+  return (
+    <>
+      <img src={src} width={width} height={height} className={cn(inputVariants({ className }))} {...props} />
+    </>
+  );
+};
