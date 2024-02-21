8c6bfbe1c32cf00b5c6756659ba94fa7cc719979
Alper Erdogan <alpererdogan8@gmail.com>
2024-02-21 16:46:07 +0300

    feat(logo): add link to home in Logo component

--- frontend/src/assets/logo.tsx
+++ frontend/src/assets/logo.tsx
@@ -7,8 +7,10 @@ interface LogoProps extends SVGProps<SVGSVGElement> {
 
 export const Logo: FC<LogoProps> = ({ fill, ...props }) => {
   return (
-    <svg width="64" height="64" viewBox="0 0 64 64" fill="current" xmlns="http://www.w3.org/2000/svg" {...props}>
-      <circle cx="32" cy="32" r="32" fill={fill} />
-    </svg>
+    <a href="/">
+      <svg width="64" height="64" viewBox="0 0 64 64" fill="current" xmlns="http://www.w3.org/2000/svg" {...props}>
+        <circle cx="32" cy="32" r="32" fill={fill} />
+      </svg>
+    </a>
   );
 };
