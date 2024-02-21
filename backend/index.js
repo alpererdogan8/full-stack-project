c4d9ddcb2b48586ae56b2b426941b378f8f42754
Alper Erdogan <alpererdogan8@gmail.com>
2024-02-21 04:27:59 +0300

    feat: added rate limiting middleware to the Express application for enhanced security against too many requests.

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,19 @@
 import express from "express";
+import { rateLimit } from "express-rate-limit";
 import albums from "./routes/albums.js";
 import search from "./routes/search.js";
-
 const PORT = process.env.PORT;
 
 const app = express();
+const limiter = rateLimit({
+  message: "too many requests, please wait a bit",
+  windowMs: 10000, // 10 second
+  limit: 5,
+  standardHeaders: "draft-7",
+  legacyHeaders: true,
+});
+
+app.use(limiter);
 
 app.use("/", search);
 app.use("/albums", albums);
