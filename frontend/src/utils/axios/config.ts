a018512915530291f07c078c62118c38703cdfbc
Alper Erdogan <alpererdogan8@gmail.com>
2024-02-21 18:03:36 +0300

    feat(axios): added API instance with dynamic baseURL

---
+++ frontend/src/utils/axios/config.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+const apiUrl: string =
+  import.meta.env.MODE === "production"
+    ? import.meta.env.VITE_REACT_APP_API_URL_PROD!
+    : import.meta.env.VITE_REACT_APP_API_URL_DEV!;
+
+export const API = axios.create({
+  baseURL: apiUrl,
+  headers: { "Content-Type": "application/json", Accept: "application/json" },
+  withCredentials: true,
+});
