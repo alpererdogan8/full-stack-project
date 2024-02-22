2aa7aa8fa1056cc199388300aae68b5140f5ca82
Alper Erdogan <alpererdogan8@gmail.com>
2024-02-22 03:03:14 +0300

    feat(api&context): implemented and created ContextAPIProvider and related functions

--- frontend/src/contextAPI/ContextAPI.tsx
+++ frontend/src/contextAPI/ContextAPI.tsx
@@ -1,7 +1,91 @@
-import { FC, ReactNode, createContext } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { FC, ReactNode, Reducer, createContext, useEffect, useReducer, useState } from "react";
+import { API } from "../utils/axios/config";
+import { reducer } from "../utils/reducers/reducer";
+import type { ApiResponse, Details, InitialState, TypeAction } from "../utils/reducers/types";
 
-export const ContextAPI = createContext({});
+interface IContextAPI {
+  contextAPIState: InitialState;
+  getAlbums: (page?: number) => Promise<void> | undefined;
+  getSingleAlbum: (albumId: number, page?: number) => Promise<void> | undefined;
+  getSingleAlbumDetails: (albumId: number, photoId: number) => Promise<void> | undefined;
+  details: Details;
+}
+
+const ApiResponse: ApiResponse = {
+  limit: 25,
+  page: 1,
+  results: [],
+  totalItems: 0,
+  totalPages: 0,
+};
+
+export const ContextAPI = createContext<IContextAPI>({
+  contextAPIState: { data: ApiResponse, loading: true, error: "" },
+  getAlbums: () => Promise.resolve() || undefined,
+  getSingleAlbum: () => Promise.resolve() || undefined,
+  getSingleAlbumDetails: () => Promise.resolve() || undefined,
+  details: {
+    albumId: 0,
+    comments: [],
+    id: 0,
+    thumbnailUrl: "",
+    title: "",
+    url: "",
+  },
+});
+const initialState: InitialState = {
+  data: ApiResponse,
+  error: null,
+  loading: true,
+};
 
 export const ContextAPIProvider: FC<{ children: ReactNode }> = ({ children }) => {
-  return <ContextAPI.Provider value={{ data: "hey" }}>{children}</ContextAPI.Provider>;
+  const [contextAPIState, dispatch] = useReducer<Reducer<InitialState, TypeAction>>(reducer, initialState);
+  const [details, setDetails] = useState<Details>({
+    albumId: 0,
+    comments: [],
+    id: 0,
+    thumbnailUrl: "",
+    title: "",
+    url: "",
+  });
+
+  const getAlbums = async (page?: number) => {
+    try {
+      dispatch({ type: "FETCH_START" });
+      const { data } = await API.get(`/albums?page=${page}`);
+
+      dispatch({ type: "FETCH_SUCCESS", payload: data });
+    } catch (error) {
+      dispatch({ type: "FETCH_ERROR", payload: error as string });
+    }
+  };
+  const getSingleAlbum = async (albumId: number, page?: number) => {
+    try {
+      dispatch({ type: "FETCH_START" });
+      const { data } = await API.get(`/albums/${albumId}?page=${page}`);
+      console.log(data);
+
+      dispatch({ type: "FETCH_SUCCESS", payload: data });
+    } catch (error) {
+      dispatch({ type: "FETCH_ERROR", payload: error as string });
+    }
+  };
+  const getSingleAlbumDetails = async (albumId: number, photoId: number) => {
+    try {
+      const { data } = await API.get(`/albums/${albumId}/photos/${photoId}`);
+      setDetails({ ...data });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    console.log(details);
+  }, [contextAPIState]);
+  return (
+    <ContextAPI.Provider value={{ contextAPIState, getAlbums, getSingleAlbum, getSingleAlbumDetails, details }}>
+      {children}
+    </ContextAPI.Provider>
+  );
 };
