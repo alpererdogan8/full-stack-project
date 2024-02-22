0cbe8fa2683bf04c8e3adbbb2c36c286e6690703
Alper Erdogan <alpererdogan8@gmail.com>
2024-02-22 03:05:57 +0300

    style(card): improve Card component layout and responsiveness

--- frontend/src/components/molecules/card.tsx
+++ frontend/src/components/molecules/card.tsx
@@ -8,11 +8,23 @@ type TypeCard = {
 };
 
 const Card = ({ src, title, href }: TypeCard) => {
+  if (src) {
+    return (
+      <div className="  w-[350px] h-[150px] m-2 flex rounded-md justify-between gap-2 p-3 border-border shadow-md   text-wrap whitespace-break-spaces border-2">
+        <Image width="120px" height="120px" src={src} />
+        <div className="flex flex-col  justify-between items-end ">
+          <h2 className="text-lg font-semibold px-4 line-clamp-2">{title}</h2>
+          <Button href={href} size={"icon"} className="w-full sm:w-auto" variantType={"link"}>
+            Go to Details
+          </Button>
+        </div>
+      </div>
+    );
+  }
   return (
-    <div className=" m-2 flex rounded-md justify-between gap-2 p-3 border-border shadow-md  h-auto text-wrap whitespace-break-spaces border-2">
-      {typeof src !== "undefined" ? <Image width="120px" src={src} /> : null}
-      <div className="flex flex-col  justify-between items-end ">
-        <h2 className="text-lg font-semibold">{title}</h2>
+    <div className=" w-56 h-32 m-2 flex rounded-md justify-between gap-2 px-1 py-2 border-border shadow-md hover:shadow-lg text-wrap whitespace-break-spaces border-2">
+      <div className="flex flex-col w-full   justify-between items-end ">
+        <h2 className="text-base w-full font-semibold">{title}</h2>
         <Button href={href} size={"icon"} className="w-full sm:w-auto" variantType={"link"}>
           Go to Details
         </Button>
