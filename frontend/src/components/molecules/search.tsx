ef951721816d57db37b38c0ee59eb1b17ed62370
Alper Erdogan <alpererdogan8@gmail.com>
2024-02-22 08:22:49 +0300

    refactor(search): update Search component with input handling

--- frontend/src/components/molecules/search.tsx
+++ frontend/src/components/molecules/search.tsx
@@ -1,12 +1,16 @@
 import { SearchIcon } from "lucide-react";
+import { useState } from "react";
+import { useAPI } from "../../hooks/useAPI";
 import { Button } from "../atomic/button";
 import { Input } from "../atomic/input";
 
 const Search = () => {
+  const [input, setInput] = useState<string>("");
+  const { handleSearch } = useAPI();
   return (
     <div className="flex items-center gap-2">
-      <Input />
-      <Button>
+      <Input onChange={(e) => setInput(e.target.value)} />
+      <Button onClick={() => handleSearch(input as string)}>
         <SearchIcon />
       </Button>
     </div>
