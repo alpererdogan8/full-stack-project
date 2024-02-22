6daeb9638c670ca14a2b2340da5ccfea2c184073
Alper Erdogan <alpererdogan8@gmail.com>
2024-02-22 03:06:39 +0300

    refactor(details): enhanced Details component and integrate with useAPI

--- frontend/src/components/pages/details.tsx
+++ frontend/src/components/pages/details.tsx
@@ -1,45 +1,34 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useEffect } from "react";
+import { useParams } from "react-router";
+import { useAPI } from "../../hooks/useAPI";
 import Comments from "../molecules/comments";
 
 const Details = () => {
+  const { getSingleAlbumDetails, details } = useAPI();
+  const { albumId, detailId } = useParams();
+  useEffect(() => {
+    try {
+      (async () => {
+        return await getSingleAlbumDetails(Number(albumId), Number(detailId));
+      })();
+    } catch (error) {
+      console.log(error);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <div className="w-full  mt-10  flex flex-col items-center gap-20 ">
-      <h2 className="font-semibold text-4xl">HEADER</h2>
-      <img src="https://via.placeholder.com/600/771796" className="w-[516px]" />
+      <h2 className="font-semibold text-4xl">{details.title}</h2>
+      <img src={details.url} className="w-[516px]" />
       <div className="flex w-11/12 md:w-8/12 flex-col ">
         <h2 className="text-2xl w-full text-center md:text-start font-semibold mb-8">Comments</h2>
         <div className="flex w-full  gap-5 flex-col items-center">
-          <Comments
-            author="alpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd  "
-            comment="sdşkgnsdişkngsd şdskgn kngsşgisdkns dşks işgknsşnk lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd  lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd  lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd  lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd  lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd "
-          />
-          <Comments
-            author="alpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd  "
-            comment="sdşkgnsdişkngsd şdskgn kngsşgisdkns dşks işgknsşnk lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd  lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd  lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd  lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd  lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd "
-          />
-          <Comments
-            author="alpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd  "
-            comment="sdşkgnsdişkngsd şdskgn kngsşgisdkns dşks işgknsşnk lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd  lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd  lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd  lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd  lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd "
-          />
-          <Comments
-            author="alpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd  "
-            comment="sdşkgnsdişkngsd şdskgn kngsşgisdkns dşks işgknsşnk lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd  lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd  lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd  lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd  lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd "
-          />
-          <Comments
-            author="alpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd  "
-            comment="sdşkgnsdişkngsd şdskgn kngsşgisdkns dşks işgknsşnk lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd  lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd  lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd  lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd  lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd "
-          />
-          <Comments
-            author="alpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd  "
-            comment="sdşkgnsdişkngsd şdskgn kngsşgisdkns dşks işgknsşnk lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd  lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd  lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd  lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd  lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd "
-          />
-          <Comments
-            author="alpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd  "
-            comment="sdşkgnsdişkngsd şdskgn kngsşgisdkns dşks işgknsşnk lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd  lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd  lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd  lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd  lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd "
-          />
-          <Comments
-            author="alpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd  "
-            comment="sdşkgnsdişkngsd şdskgn kngsşgisdkns dşks işgknsşnk lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd  lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd  lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd  lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd  lpersd g sd gd s gds gdgdgdgd gd  gdgdgdgdgd "
-          />
+          {details &&
+            details.comments &&
+            details?.comments.map((comment: any) => {
+              return <Comments key={comment.id} author={comment.name} comment={comment.body} />;
+            })}
         </div>
       </div>
     </div>
