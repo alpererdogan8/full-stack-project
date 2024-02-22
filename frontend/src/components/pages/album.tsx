4093bae21e546eea5a03c020bf57047ffbf1bcb5
Alper Erdogan <alpererdogan8@gmail.com>
2024-02-22 03:07:34 +0300

    refactor(albums&album): integrated useAPI hook in Albums and Album component

--- frontend/src/components/pages/album.tsx
+++ frontend/src/components/pages/album.tsx
@@ -1,20 +1,41 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect } from "react";
+import { useLocation, useParams } from "react-router";
+import { useAPI } from "../../hooks/useAPI";
 import Card from "../molecules/card";
 
 const Album = () => {
+  const { albumId } = useParams();
+  const { search } = useLocation();
+  const queryParams = new URLSearchParams(search);
+  const page = queryParams.get("page");
+  const { contextAPIState, getSingleAlbum } = useAPI();
+  useEffect(() => {
+    try {
+      (async () => {
+        return await getSingleAlbum(Number(albumId), Number(page));
+      })();
+      console.log(contextAPIState);
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
   return (
     <div className="w-11/12 mt-10 h-auto gap-2 flex flex-col md:flex-row md:flex-wrap md:justify-center md:items-start items-center ">
-      <Card title="quidem molestiae enim" href="#" src="https://via.placeholder.com/600/771796" />
-      <Card title="quidem molestiae enim" href="#" src="https://via.placeholder.com/600/771796" />
-      <Card title="quidem molestiae enim" href="#" src="https://via.placeholder.com/600/771796" />
-      <Card title="quidem molestiae enim" href="#" src="https://via.placeholder.com/600/771796" />
-      <Card title="quidem molestiae enim" href="#" src="https://via.placeholder.com/600/771796" />
-      <Card title="quidem molestiae enim" href="#" src="https://via.placeholder.com/600/771796" />
-      <Card title="quidem molestiae enim" href="#" src="https://via.placeholder.com/600/771796" />
-      <Card title="quidem molestiae enim" href="#" src="https://via.placeholder.com/600/771796" />
-      <Card title="quidem molestiae enim" href="#" src="https://via.placeholder.com/600/771796" />
-      <Card title="quidem molestiae enim" href="#" src="https://via.placeholder.com/600/771796" />
-      <Card title="quidem molestiae enim" href="#" src="https://via.placeholder.com/600/771796" />
-      <Card title="quidem molestiae enim" href="#" src="https://via.placeholder.com/600/771796" />
+      {contextAPIState && contextAPIState.loading
+        ? "loading"
+        : contextAPIState.data.results?.map((items: any) => {
+            return (
+              <Card
+                key={items.id}
+                title={items.title}
+                href={`/albums/${items.albumId}/details/${items.id}`}
+                src={items.url}
+              />
+            );
+          })}
     </div>
   );
 };
--- frontend/src/components/pages/albums.tsx
+++ frontend/src/components/pages/albums.tsx
@@ -1,20 +1,33 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useEffect } from "react";
+import { useLocation } from "react-router";
+import { useAPI } from "../../hooks/useAPI";
 import Card from "../molecules/card";
 
 const Albums = () => {
+  const { contextAPIState, getAlbums } = useAPI();
+  const { search } = useLocation();
+
+  const queryParams = new URLSearchParams(search);
+  const page = queryParams.get("page");
+
+  useEffect(() => {
+    try {
+      (async () => {
+        return await getAlbums(Number(page));
+      })();
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
   return (
     <div className="w-11/12 my-10 h-auto gap-2 flex flex-col md:flex-row md:flex-wrap md:justify-center md:items-start items-center ">
-      <Card title="quidem molestiae enim" href="#" />
-      <Card title="quidem molestiae enim" href="#" />
-      <Card title="quidem molestiae enim" href="#" />
-      <Card title="quidem molestiae enim" href="#" />
-      <Card title="quidem molestiae enim" href="#" />
-      <Card title="quidem molestiae enim" href="#" />
-      <Card title="quidem molestiae enim" href="#" />
-      <Card title="quidem molestiae enim" href="#" />
-      <Card title="quidem molestiae enim" href="#" />
-      <Card title="quidem molestiae enim" href="#" />
-      <Card title="quidem molestiae enim" href="#" />
-      <Card title="quidem molestiae enim" href="#" />
+      {contextAPIState && contextAPIState.loading
+        ? "loading"
+        : contextAPIState.data.results.map((items: any) => {
+            return <Card key={items.id} title={items.title} href={`/albums/${items.id}`} />;
+          })}
     </div>
   );
 };
