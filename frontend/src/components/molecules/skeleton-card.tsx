8e30f4bd10d98292c4fd78f4ff3a175874c5e6c1
Alper Erdogan <alpererdogan8@gmail.com>
2024-02-22 11:44:56 +0300

    feat: Add new SkeletonCard component

---
+++ frontend/src/components/molecules/skeleton-card.tsx
@@ -0,0 +1,44 @@
+const SkeletonCard = () => {
+  return (
+    <>
+      <div
+        className=" w-56 h-32 m-2 flex rounded-md border-border
+       shadow-md hover:shadow-lg bg-slate-300 animate-pulse "></div>
+      <div
+        className=" w-56 h-32 m-2 flex rounded-md border-border
+       shadow-md hover:shadow-lg bg-slate-300 animate-pulse "></div>
+      <div
+        className=" w-56 h-32 m-2 flex rounded-md border-border
+       shadow-md hover:shadow-lg bg-slate-300 animate-pulse "></div>
+      <div
+        className=" w-56 h-32 m-2 flex rounded-md border-border
+       shadow-md hover:shadow-lg bg-slate-300 animate-pulse "></div>
+      <div
+        className=" w-56 h-32 m-2 flex rounded-md border-border
+       shadow-md hover:shadow-lg bg-slate-300 animate-pulse "></div>
+      <div
+        className=" w-56 h-32 m-2 flex rounded-md border-border
+       shadow-md hover:shadow-lg bg-slate-300 animate-pulse "></div>
+      <div
+        className=" w-56 h-32 m-2 flex rounded-md border-border
+       shadow-md hover:shadow-lg bg-slate-300 animate-pulse "></div>
+      <div
+        className=" w-56 h-32 m-2 flex rounded-md border-border 
+      shadow-md hover:shadow-lg bg-slate-300 animate-pulse "></div>
+      <div
+        className=" w-56 h-32 m-2 flex rounded-md border-border
+       shadow-md hover:shadow-lg bg-slate-300 animate-pulse "></div>
+      <div
+        className=" w-56 h-32 m-2 flex rounded-md border-border
+       shadow-md hover:shadow-lg bg-slate-300 animate-pulse "></div>
+      <div
+        className=" w-56 h-32 m-2 flex rounded-md border-border
+       shadow-md hover:shadow-lg bg-slate-300 animate-pulse "></div>
+      <div
+        className=" w-56 h-32 m-2 flex rounded-md border-border
+       shadow-md hover:shadow-lg bg-slate-300 animate-pulse "></div>
+    </>
+  );
+};
+
+export default SkeletonCard;
